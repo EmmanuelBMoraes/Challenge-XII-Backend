9e104d161bdc35d7bdf03632ab6f9a67b933515b
Emmanuel-Moraes <emmanuelbmoraes@gmail.com>
2024-05-13 09:41:43 -0300

    feat: create posts service

---
+++ src/posts/dto/posts.dto.ts
@@ -0,0 +1,8 @@
+export class PostsDto {
+  readonly id?: string;
+  readonly title: string;
+  readonly imageUrl: string;
+  readonly description: string;
+  readonly createdAt?: string;
+  readonly updatedAt?: string;
+}
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Posts } from 'src/entities/post.entity';
+import { Repository } from 'typeorm';
+import { PostsDto } from './dto/posts.dto';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(
+    @InjectRepository(Posts)
+    private readonly postsRepository: Repository<Posts>,
+  ) {}
+
+  async getAllPosts(): Promise<Posts[]> {
+    const posts = await this.postsRepository.find();
+    return posts;
+  }
+
+  async createNewPost(post: PostsDto): Promise<PostsDto> {
+    try {
+      const newPost = await this.postsRepository.save(post);
+      return newPost;
+    } catch (error) {
+      return Promise.reject();
+    }
+  }
+}
